c454c28b9f9ad024cf1b2c11a24bdcfbde87a83d
vinod.pahuja <vinod.pahuja@1c88fc3c-09b5-11df-a26f-8b716316a5bc>
2010-08-11 07:16:53 +0000

    Persistence RnD

--- PRnD/src/rnd/op/TestDNAP.java
+++ PRnD/src/rnd/op/TestDNAP.java
@@ -3,6 +3,9 @@ package rnd.op;
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManagerFactory;
 
+import rnd.op.dnap.DNAPObjectPersistor;
+import aa.server.User;
+
 public class TestDNAP {
 
 	public static void main(String[] args) {
@@ -10,8 +13,14 @@ public class TestDNAP {
 		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory(TestDNAP.class.getClassLoader().getResourceAsStream("pmf.properties"));
 		System.out.println(pmf);
 
-//		 DNAPObjectPersistor persistor = new DNAPObjectPersistor(pmf);
-//		 persistor.saveObject(new User());
+		ObjectPersistor persistor = new DNAPObjectPersistor(pmf);
+
+		User user = new User();
+		user.setUserName("vinod.pahuja");
+
+		persistor.saveObject(user);
+
+		// System.out.println(new User() instanceof PersistenceCapable);
 
 	}
 }
